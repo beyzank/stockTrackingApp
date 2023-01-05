a51b6b9527775376bca62ac925188e319e68f6bb
Beyza Nur Kahraman <beyzanurkahraman.11@gmail.com>
2023-01-06 01:05:51 +0300

    add product

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
 
 export default function App() {
+  const [serialNo, setSerialNo] = useState();
+  const [productName, setProductName] = useState("");
+  const [count, setCount] = useState();
+
 
 // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -13,15 +18,53 @@ export default function App() {
     projectId: "idvlabs-stock-tracking-app",
     storageBucket: "idvlabs-stock-tracking-app.appspot.com",
     messagingSenderId: "421003806366",
-    appId: "1:421003806366:web:f108816a2250bf361bdd8f"
+    appId: "1:421003806366:web:f108816a2250bf361bdd8f",
+    databaseURL: "https://idvlabs-stock-tracking-app-default-rtdb.europe-west1.firebasedatabase.app",
   };
 
 // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+
+  },[]);
+
+  const writeProductData = () => {
+    const db = getDatabase();
+    set(ref(db, 'products/' + serialNo), {
+      serialNo: serialNo,
+      productName: productName,
+      count: count
+    });
+  }
   return (
     <View style={styles.container}>
       <Text>Stock Tracking App</Text>
-      <StatusBar style="auto" />
+      <Text>Seri No(*)</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setSerialNo}
+            value={serialNo}
+            placeholder=""
+            keyboardType="numeric"
+        />
+        <Text>Ürün Adı(*)</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setProductName}
+            value={productName}
+            placeholder=""
+            keyboardType="numeric"
+        />
+        <Text>Stok Adedi(*)</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setCount}
+            value={count}
+            placeholder=""
+            keyboardType="numeric"
+        />
+      <TouchableOpacity onPress={writeProductData}>Kaydet</TouchableOpacity>
     </View>
   );
 }
